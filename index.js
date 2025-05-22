468708e4361a74abf5ec46dc3fa48cd7ce67897d
suraiya-jahan-bhuiyan-sraboni <suraiya.jaha.bhuiyan.12@gmail.com>
2025-05-22 17:15:51 +0600

    adding some npm packages

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 3000
 
+app.use(cors())
+app.use(express.json())
+
 app.get('/', (req, res) => {
-    res.send('Hello World!s')
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
