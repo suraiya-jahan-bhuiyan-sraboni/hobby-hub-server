0db4ca6ea90a931b800f02c40f9cc248b273ba8a
suraiya-jahan-bhuiyan-sraboni <suraiya.jaha.bhuiyan.12@gmail.com>
2025-05-23 01:09:50 +0600

    update group implemented

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json())
 
 require('dotenv').config()
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.lln6rsh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,17 +26,56 @@ async function run() {
         await client.connect();
 
         app.get('/groups', async (req, res) => {
-            const cursor = client.db('HobbyHubDb').collection('groups').find();
-            const groups = await cursor.toArray();
-            res.send(groups);
+
+            const email = req.query.email;
+            const collection = client.db('HobbyHubDb').collection('groups');
+
+            try {
+                let groups;
+                if (email) {
+                    groups = await collection.find({ userEmail: email }).toArray();
+                } else {
+                    groups = await collection.find().toArray();
+                }
+                res.send(groups);
+            } catch (err) {
+                res.status(500).send({ error: 'Failed to fetch groups' });
+            }
+        });
+        app.get('/groups/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const group = await client.db('HobbyHubDb').collection('groups').findOne(query);
+            res.send(group);
         });
         
+
         app.post('/createGroup', async (req, res) => {
             const group = req.body;
             //console.log(group);
             const result = await client.db('HobbyHubDb').collection('groups').insertOne(group);
             res.send(result);
            
+        });
+        app.put('/updateGroup/:id', async (req, res) => {
+            const id = req.params.id;
+            const {name,category, description,maxMembers,startDate, imageUrl,userName,userEmail} = req.body;
+            const query = { _id: new ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    name,
+                    category,
+                    description,
+                    maxMembers,
+                    startDate,
+                    imageUrl,
+                    userName,
+                    userEmail
+                },
+            };
+            const result = await client.db('HobbyHubDb').collection('groups').updateOne(query, updateDoc, options);
+            res.send(result);
         });
 
         // Send a ping to confirm a successful connection
