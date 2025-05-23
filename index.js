b1e2075cd93e6b55076c413a7f93ae6f2f474fb1
suraiya-jahan-bhuiyan-sraboni <suraiya.jaha.bhuiyan.12@gmail.com>
2025-05-23 13:07:37 +0600

    fixing

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ app.use(express.json());
 
 
 
-//const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.lln6rsh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-const uri = process.env.MONGO_URI;
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.lln6rsh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+//const uri = process.env.MONGO_URI;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
